a098537780092bb0914ce477e0b53f6afa92b188
gabriellejohn1030 <gabriellejohn1030@yahoo.com>
2020-01-14 09:12:12 -0800

    Modified test.cpp file

--- test.cpp
+++ test.cpp
@@ -14,13 +14,13 @@ TEST(EchoTest, MyNameIsGabby) {
 	char* test_val[5]; test_val[0] = "./c-echo"; test_val[1] = "my"; test_val[2] = "name"; test_val[3] = "is"; test_val[4] = "gabby";
 	EXPECT_EQ("my name is gabby", echo(5,test_val));
 }
-TEST(EchoTest, Angelene) {
-	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "angelene";
-	EXPECT_EQ("angelene", echo(2,test_val));
+TEST(EchoTest, gabriellejohnyahoo) {
+	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "gabriellejohn!yahoo";
+	EXPECT_EQ("gabriellejohn!yahoo", echo(2,test_val));
 }
-TEST(EchoTest,PeriodT) {
-	char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "period"; test_val[2] = "t";
-	EXPECT_EQ("period t",echo(3,test_val));
+TEST(EchoTest,916Area) {
+	char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "916"; test_val[2] = "Area";
+	EXPECT_EQ("916 Area",echo(3,test_val));
 }
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
